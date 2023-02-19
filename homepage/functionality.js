b7823d3a41107bace9bac412d899a1d7a4231c04
Matthew Sherwood <105366175+sMattSherwood@users.noreply.github.com>
2023-02-18 20:27:56 -0500

    added more information to the footer

--- homepage/functionality.js
+++ homepage/functionality.js
@@ -1,5 +1,15 @@
-var options = []
-function help()
-{
-    alert("button pressed");
-}
+    // adding the window load will allow the date to wait for the page to load then display the date
+    window.addEventListener("load", function(){
+
+        var date = new Date() // creates a date object 
+        // this will get current day/month/year
+        var day = date.getDate()
+        var month = date.getMonth() + 1 // you add 1 to this since it's pulling from an array where janary will be stored at 0
+        var year = date.getFullYear()
+        // this will store the data as day-month-year
+        var todaysDate = day + "/" + month + "/" + year
+        console.log(todaysDate)// check to see if it's displaying the date
+        //dipslays the date in the paragraph tag
+        document.getElementById("currentDate").innerHTML = todaysDate
+
+    })
