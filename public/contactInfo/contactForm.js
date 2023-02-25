d9c860c57ab13494361371c7eec8211a1926c8e0
Matthew Sherwood <105366175+sMattSherwood@users.noreply.github.com>
2023-02-24 23:34:34 -0500

    debuged the properties of null reading

--- public/contactInfo/contactForm.js
+++ public/contactInfo/contactForm.js
@@ -1,9 +1,22 @@
-window.addEventListener("load", function(){
-
+document.addEventListener('DOMContentLoaded', () =>{   
     const contactForm = document.querySelector('.contact-form')
+
+    let name = document.getElementById("name")
+    let email = document.getElementById("email")
+    let subject = document.getElementById("subject")
+    let message = document.getElementById("message")
 
     contactForm.addEventListener('submit', (e)=>{
         e.preventDefault()
-        console.log("hello button is working")
+
+        let formData = {
+            name: name.value,
+            email: email.value,
+            subject: subject.value,
+            message: message.value
+        }
+
+        console.log(formData)
     })
-})
+
+}) 
