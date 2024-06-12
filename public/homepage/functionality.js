ed8aef6d366b352b8211d0931ab6f61632ded552
sMattSherwood <matthew.sherwood@student.sl.on.ca>
2024-06-12 15:12:28 -0400

    created a way to get rid of css grid display using javascript document.create element

--- public/homepage/functionality.js
+++ public/homepage/functionality.js
@@ -1,15 +1,42 @@
     // adding the window load will allow the date to wait for the page to load then display the date
-    window.addEventListener("load", function(){
+    
+    displayCss();
+    window.addEventListener("load", function displayDate(){
 
-        var date = new Date() // creates a date object 
+        var date = new Date(); // creates a date object 
         // this will get current day/month/year
-        var day = date.getDate()
-        var month = date.getMonth() + 1 // you add 1 to this since it's pulling from an array where janary will be stored at 0
-        var year = date.getFullYear()
+        var day = date.getDate();
+        var month = date.getMonth() + 1; // you add 1 to this since it's pulling from an array where janary will be stored at 0
+        var year = date.getFullYear();
         // this will store the data as day-month-year
-        var todaysDate = day + "/" + month + "/" + year
-        console.log(todaysDate)// check to see if it's displaying the date
+        var todaysDate = day + "/" + month + "/" + year;
+        console.log(todaysDate);// check to see if it's displaying the date
         //dipslays the date in the paragraph tag
-        document.getElementById("currentDate").innerHTML = todaysDate
+        document.getElementById("currentDate").innerHTML = todaysDate;
+
+        
+
+        
 
     })
+
+    function displayCss(){
+        // this will apply Css depending if it's on a Moble Phone or Desktop Computer
+        let width = window.innerWidth;
+        // this will display the mobile css
+        if(width <= 550) {
+            var link = document.createElement("link");
+            link.type = "text/css";
+            link.rel = "Stylesheet";
+            link.href = "mobileStyles.css";
+            document.head.appendChild(link);
+        }
+        // this will put the css for the desktop web applicaiton
+        else {
+            var link = document.createElement("link");
+            link.type = "text/css";
+            link.rel = "Stylesheet";
+            link.href = "styles.css";
+            document.head.appendChild(link);
+        }
+    }
